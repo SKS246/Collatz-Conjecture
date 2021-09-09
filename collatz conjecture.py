9dc7bdb9368faa7ceeaa1e93c85ed0099b30784a
Sks301 <soham.sinha@oberoi-is.net>
2021-09-09 15:12:17 +0530

    Create collatz conjecture.py

---
+++ collatz conjecture.py
@@ -0,0 +1,14 @@
+x = int(input("Number: "))
+iterations = 0
+
+
+while x != 1:
+    if x % 2 == 1:
+        x = (3 * x) + 1
+        print(x)
+    elif x % 2 == 0:
+        x = x/2
+        print(x)
+    iterations += 1
+
+print("Iterations: " + str(iterations))
